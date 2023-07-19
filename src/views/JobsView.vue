d0018a7b96b0aa0d7c325e406cd2f8be117f0381
Marcelearn <jaworskimarcel@gmail.com>
2023-07-19 20:35:39 +0200

    jobs list

--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -8,7 +8,37 @@ const jobs: IJob[] = (await axios.get('https://edwardtanguay.vercel.app/share/jo
 </script>
 <template>
   <div class="jobs">
-    <h2>This is an Vue3 boilerplate / jobs view</h2>
+    <h2>Jobs</h2>
     <p>There are {{ jobs.length }} jobs.</p>
+    <ul>
+      <li v-for="job in jobs">
+        <a target="_blank" :href="job.url">{{ job.title }}</a> - {{ job.company }}</li>
+    </ul>
   </div>
 </template>
+
+<style scoped>
+.jobs {
+  margin: auto;
+  max-width: 700px;
+  padding-inline: 1rem;
+}
+h2 {
+  color: rgb(255, 200, 0);
+}
+ 
+p {
+  color: #aaa;
+  font-style: italic;
+  text-align: left;
+}
+
+li {
+  color: #ccc;
+  font-size: 1.3rem;
+  text-align: left;
+}
+a {
+  color: rgb(47, 106, 255);
+}
+</style>
