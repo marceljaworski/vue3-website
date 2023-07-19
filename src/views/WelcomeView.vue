31ab7f4b9ca6874cbc8b427885ff560bf4cba6cf
Marcelearn <jaworskimarcel@gmail.com>
2023-07-19 20:56:32 +0200

    finished

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -20,17 +20,47 @@ const changeMessage = () => {
     <div class="welcome">
         <h2>{{ greeting }}</h2>
         <p>{{ message }}</p>
-        <button v-if="showButton" @click="changeMessage">more info</button>
+        <div class="button-container" >
+            <button class="border-gradient border-gradient-green" v-if="showButton" @click="changeMessage">more info</button>
+            
+        </div>
     </div>
 </template>
 
 <style scoped>
     h2 {
         color: rgb(255, 200, 0);
+        margin-bottom: 2rem;
     }
  
     p {
         color: #aaa;
         font-style: italic;
+        margin-bottom: 2rem;
+    }
+
+    button {
+      background: none;
+      text-decoration: inherit;
+      font-family: system-ui;
+      font-size: 1rem;
+      padding: 1rem 2rem;
+      color: white;
+      text-transform: uppercase;
+    }
+    .button-container {
+        width: 100%;
+        padding: 1rem;
+    }
+    .border-gradient {
+      border: 10px solid;
+      border-image-slice: 1;
+      border-width: 5px;
+    }
+    .border-gradient-purple {
+      border-image-source: linear-gradient(to left, #743ad5, #d53a9d);
+    }
+    .border-gradient-green {
+      border-image-source: linear-gradient(to left, #00C853, #B2FF59);
     }
 </style>
